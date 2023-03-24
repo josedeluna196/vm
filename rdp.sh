b08ce68097f4aeb021f3336551eda54e029a4d74
josedeluna196 <122432156+josedeluna196@users.noreply.github.com>
2023-03-23 20:04:47 -0500

    Update rdp.sh

--- rdp.sh
+++ rdp.sh
@@ -11,6 +11,6 @@ echo "in - India (Mumbai)"
 read -p "choose ngrok region: " CRP
 ./ngrok tcp --region $CRP 3389 &>/dev/null &
 echo "==============================="
-sleep 2
+sleep 4
 echo "La dirección de ngrok es: $(curl -sS localhost:4040/api/tunnels | jq -r '.tunnels[0].public_url')"
 echo "===================================="
